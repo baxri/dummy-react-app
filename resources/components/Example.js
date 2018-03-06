73b2ca458bd44b23cf503dd36cc0988b72345618
Giorgi <giorgi.bibilashvili89@gmail.com>
2018-03-06 21:41:33 +0400

    --

--- resources/components/Example.js
+++ resources/components/Example.js
@@ -19,7 +19,7 @@ export default class Example extends Component {
     render() {
         return (
             <div>
-                <p><label>Edit</label></p>
+                <p><label>Enter Some Text</label></p>
                 <input onChange={(e) => this.changeEvent(e)} value={this.state.fullname} />
                 <p>{this.state.fullname}</p>
             </div>
